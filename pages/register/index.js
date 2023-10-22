b4fbd62ab19aba8e5b49ad628a274fc525a78819
Your Name <you@example.com>
2023-10-22 16:05:07 +0300

    Api Eklendi

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,6 +1,8 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 import { useState } from "react";
+import axios from "axios";
 export default function RegisterPage() {
   // const [name, setName] = useState("");
   // const [surName, setsurName] = useState("");
@@ -8,6 +10,9 @@ export default function RegisterPage() {
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
   // const [password2, setPassword2] = useState("");
+
+  //  CheckBox State'i
+  const [termsCheck, setTermsCheck] = useState(false);
 
   const [form, setForm] = useState({
     name: "",
@@ -18,9 +23,38 @@ export default function RegisterPage() {
     password2: "",
   });
 
+  // tek bir fonksiyonda değiştirme
+  const handleOnChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const createAccountService = async () => {
+    // http://localhost:3000/auth/register
+
+    const requestBody = {
+      name: form.name,
+      lastname: form.surname,
+      username: form.username,
+      email: form.email,
+      password: form.password,
+    };
+    axios.post("http://localhost:3000/auth/register", requestBody);
+  };
+
+  const handleSubmit = async () => {
+    if (!termsCheck) {
+      alert("Lütfen tikleyin");
+    } else if (form.password !== form.password2) {
+      alert("Şifreler Aynı Değil.");
+    } else {
+      await createAccountService();
+      alert("işlem tamamlandı.");
+    }
+  };
+
   return (
     <div className="bg-orange-200 h-screen flex justify-center items-center">
-      <div className="bg-white w-1/2 p-5 rounded-lg">
+      <div className="bg-white w-1/2 p-5 rounded-lg shadow-lg shadow-cyan-500/50">
         <h2 className="text-center text-2xl font-bold text-gray-300">
           Create new account
         </h2>
@@ -30,14 +64,21 @@ export default function RegisterPage() {
         </p>
 
         <div className="flex justify-center flex-col gap-5 mt-10">
+          {/* 
+          
+           BU KOMUT OBJELERİ DÜZ STRİNG HALİNE ÇEVİRİR.
+          {JSON.stringify(form)}
+           
+          
+          */}
+
           <div className="flex gap-5">
             <TextField
               id="outlined-basic"
               label="Name"
+              name="name"
               value={form.name}
-              onChange={(event) =>
-                setForm({ ...form, name: event.target.value })
-              }
+              onChange={(event) => handleOnChange(event)}
               variant="outlined"
               type="text"
               sx={{
@@ -48,10 +89,9 @@ export default function RegisterPage() {
             <TextField
               id="outlined-basic"
               label="Surname"
+              name="surname"
               value={form.surname}
-              onChange={(event) =>
-                setForm({ ...form, surname: event.target.value })
-              }
+              onChange={(event) => handleOnChange(event)}
               variant="outlined"
               type="text"
               sx={{
@@ -64,10 +104,9 @@ export default function RegisterPage() {
             <TextField
               id="outlined-basic"
               label="Enter your Username"
+              name="username"
               value={form.username}
-              onChange={(event) =>
-                setForm({ ...form, username: event.target.value })
-              }
+              onChange={(event) => handleOnChange(event)}
               variant="outlined"
               type="text"
               sx={{
@@ -78,10 +117,9 @@ export default function RegisterPage() {
             <TextField
               id="outlined-basic"
               label="Enter your e-mail"
+              name="email"
               value={form.email}
-              onChange={(event) =>
-                setForm({ ...form, email: event.target.value })
-              }
+              onChange={(event) => handleOnChange(event)}
               variant="outlined"
               type="email"
               sx={{
@@ -93,10 +131,9 @@ export default function RegisterPage() {
           <TextField
             id="outlined-basic"
             label="Enter your password"
+            name="password"
             value={form.password}
-            onChange={(event) =>
-              setForm({ ...form, password: event.target.value })
-            }
+            onChange={(event) => handleOnChange(event)}
             variant="outlined"
             type="password"
             sx={{
@@ -107,10 +144,9 @@ export default function RegisterPage() {
           <TextField
             id="outlined-basic"
             label=" Re-Type your password"
+            name="password2"
             value={form.password2}
-            onChange={(event) =>
-              setForm({ ...form, password2: event.target.value })
-            }
+            onChange={(event) => handleOnChange(event)}
             variant="outlined"
             type="password"
             sx={{
@@ -118,12 +154,22 @@ export default function RegisterPage() {
             }}
           />
 
-          <a href="#" className="text-end text-blue-500">
-            Forgot your Password?
-          </a>
+          <div className="flex  items-center">
+            <Checkbox
+              checked={termsCheck}
+              onChange={() => {
+                setTermsCheck(!termsCheck);
+              }}
+            />
+            <span>I Accept the privacy Policy adn Trems Of use</span>
+          </div>
 
           <Button
             variant="contained"
+            disabled={termsCheck === false}
+            onClick={() => {
+              handleSubmit();
+            }}
             className="bg-gray-900 py-3 text-lg"
             sx={{
               "&:hover": {
